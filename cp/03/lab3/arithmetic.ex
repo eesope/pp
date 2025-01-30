9602ed3276e49342eb0accde1c470c1ddab169fb
Saeyoung Park <spark363@my.bcit.ca>
2025-01-29 16:20:25 -0800

    Fix lab3 to not use send()

--- cp/03/lab3/arithmetic.ex
+++ cp/03/lab3/arithmetic.ex
@@ -1,12 +1,10 @@
 defmodule Arithmetic.Worker do
   use GenServer
 
-  # client; no communication with
   def start(_) do
     GenServer.start(__MODULE__, :no_state)
   end
 
-  # server (implementation)
   @impl true
   def init(:no_state) do
     {:ok, :no_state}
@@ -14,17 +12,19 @@ defmodule Arithmetic.Worker do
 
   @impl true
   def handle_call({:square, x}, from, :no_state) do
-    result = {x * x, self()}
-    # GenServer.cast(s_pid, {:result, from, result})
-    {:reply, result, :no_state}
+    result = x * x
+    reply = "Result: #{result} @ worker: #{inspect(self())}"
+
+    {:reply, reply, :no_state}
   end
 
   @impl true
   def handle_call({:sqrt, x}, from, :no_state) do
     Process.sleep(4000)
     result = if x >= 0, do: :math.sqrt(x), else: :error
-    # GenServer.cast(s_pid, {:result, from, result} )
-    {:reply, {result, self()}, :no_state}
+    reply = "Result is #{result} @ worker: #{inspect(self())}"
+
+    {:reply, reply, :no_state}
   end
 end
 
@@ -32,21 +32,23 @@ end
 
 defmodule Arithmetic.Server do
   use GenServer
-  # registered && doesn't wait worker to finish
 
-  # client
   def start(n \\ 1) do  # n == # of workers
     GenServer.start(__MODULE__, n, name: __MODULE__)
   end
 
   # Picked worker will do the task; answer
   def square(x) do
-    GenServer.call(__MODULE__, {:square, x})
-    IO.puts("Result: # from #{self()}")
+    {:ok, {:square, worker}} = GenServer.call(__MODULE__, {:square, x})
+    GenServer.call(worker, {:square, x})
+    # IO.puts("Result: # from #{self()}")
 
   end
   def sqrt(x) do
-    GenServer.call(__MODULE__, {:sqrt, x})
+    {:ok, {:sqrt, worker}} = GenServer.call(__MODULE__, {:sqrt, x})
+    GenServer.call(worker, {:sqrt, x})
+    # GenServer.call(w_pid, {:square, x})
+
   end
 
   # server callbacks
@@ -64,28 +66,18 @@ defmodule Arithmetic.Server do
     {:ok, state}
   end
 
-
   # pick worker -> one handle call do both job
   @impl true
   def handle_call({request, x}, _from, state = %{workers: workers, next: index, count: n}) do
     w_pid = Enum.at(workers, index)  # pick worker
-    request_res = "#{request} task is assigned to #{w_pid}"
+    w_set = {:ok, {request, w_pid}}
+
     case request do
         :square ->
-              # {:reply, GenServer.call(w_pid, {:square, x}), %{state | next: rem(index + 1, n)}}
-              {:reply, request_res, %{state | next: rem(index + 1, n)}}
+          {:reply, w_set, %{state | next: rem(index + 1, n)}}
 
         :sqrt ->
-              # {:reply, GenServer.call(w_pid, {:sqrt, x}), %{state | next: rem(index + 1, n)}}
-              {:reply, request_res, %{state | next: rem(index + 1, n)}}
-
+          {:reply, w_set, %{state | next: rem(index + 1, n)}}
     end
   end
-
-  @impl true
-  def handle_cast({:result, w_pid, result}, state) do
-    IO.puts("Result: #{result} from #{inspect(w_pid)}")
-    {:noreply, state}
-  end
-
 end
