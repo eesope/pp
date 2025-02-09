e43b12495b1f96ff238e0ed5c16eeaab666f0896
Saeyoung Park <spark363@my.bcit.ca>
2025-02-09 14:21:57 -0800

    Fix lab4-Arithmetic to use flag & trap

--- cp/04/lab4/arithmetic.ex
+++ cp/04/lab4/arithmetic.ex
@@ -1,8 +1,8 @@
 defmodule Arithmetic.Worker do
   use GenServer
 
-  def start() do
-    GenServer.start(__MODULE__, :no_state)
+  def start_link() do
+    GenServer.start_link(__MODULE__, :no_state)
   end
 
   @impl true
@@ -12,19 +12,28 @@ defmodule Arithmetic.Worker do
 
   @impl true
   def handle_call({:square, x}, _from, :no_state) do
-    result = x * x
-    reply = "Result: #{result} @worker: #{inspect(self())}"
 
-    {:reply, reply, :no_state}
+    if is_number(x) do
+      result = x * x
+      reply = "Result: #{result}   @worker: #{inspect(self())}"
+
+      {:reply, reply, :no_state}
+    else raise "Invalid argument: not a number."
+    end
+
   end
 
   @impl true
   def handle_call({:sqrt, x}, _from, :no_state) do
     Process.sleep(4000)
-    result = if x >= 0, do: :math.sqrt(x), else: :error
-    reply = "Result: #{result} @worker: #{inspect(self())}"
 
-    {:reply, reply, :no_state}
+    if is_number(x) do
+      result = if x >= 0, do: :math.sqrt(x), else: :error
+      reply = "Result: #{result} @worker: #{inspect(self())}"
+
+      {:reply, reply, :no_state}
+    else raise "Invalid argument: not a number."
+    end
   end
 end
 
@@ -51,10 +60,12 @@ defmodule Arithmetic.Server do
   # server callbacks
   @impl true
   def init(n) do
+    # to handle exit signal
+    Process.flag(:trap_exit, true)
+
     # Distribute tasks in round-robin fashion
     workers = 1..n |> Enum.map(fn _ ->
-      {:ok, pid} = Arithmetic.Worker.start()
-      Process.monitor(pid)
+      {:ok, pid} = Arithmetic.Worker.start_link()
       IO.puts("Worker: #{inspect(pid)}")
       pid
     end)
@@ -72,18 +83,26 @@ defmodule Arithmetic.Server do
     {:reply, w_set, %{state | next: rem(index + 1, n)}}
   end
 
-  # monitored worker die -> receive :DOWN msg
+  # when worker exit(terminate) -> take EXIT msg from linked process
   @impl true
-  def handle_info({:DOWN, _ref, :process, died_pid, _reason}, state = %{workers: workers, count: _n}) do
-    IO.puts("Worker #{inspect(died_pid)} died. Replacing with a new worker...")
-    {:ok, new_pid} = Arithmetic.Worker.start()
-    Process.monitor(new_pid)
-    IO.puts("New worker: #{inspect(new_pid)}")
+  def handle_info({:EXIT, died_pid, reason}, state = %{workers: workers, count: _n}) do
 
+    IO.puts(
+      "------------------------------------\n
+      Worker #{inspect(died_pid)} died. \n
+      Reason: #{inspect(reason)} \n
+      Replacing with a new worker...\n")
+
+    {:ok, new_pid} = Arithmetic.Worker.start_link()
+
+    IO.puts("
+      New worker: #{inspect(new_pid)}
+------------------------------------")
+    # update to new worker pid
     new_workers = Enum.map(workers, fn worker_pid ->
       if worker_pid == died_pid, do: new_pid, else: worker_pid
     end)
 
-    {:no_reply, %{state | workers: new_workers}}
+    {:noreply, %{state | workers: new_workers}}
   end
 end
